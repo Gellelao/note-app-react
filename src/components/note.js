9424009d14ee41160a10b4a77c98c73767a8dfaf
Deacon McIntyre <mcintydeac@ecs.vuw.ac.nz>
2018-09-22 17:06:00 +1200

    Made notes good

---
+++ src/components/note.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+    MenuProvider,
+    Menu,
+    MenuOptions,
+    MenuOption,
+    MenuTrigger,
+} from 'react-native-popup-menu';
+
+export default class Note extends React.PureComponent {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       title: '',
+    //       content: '',
+    //     }
+    //     // this.dataSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    //   }
+    state = { title: '', content: '', }
+
+    render() {
+        //   const textColor = this.props.selected ? "red" : "black";
+        title = this.props.title
+        content = this.props.content
+        return (
+            <View style={styles.container}>
+                <View style={styles.topRow}>
+                    <TextInput
+                        style={styles.title}
+                        placeholder={title}
+                        onChangeText={title => this.setState({ title })}
+                        value={this.state.title}
+                    />
+                    {/* <TouchableOpacity
+                        style={styles.moreButton}
+                        onPress={this.openMore}
+                    >
+                        <Text> ... </Text>
+                    </TouchableOpacity> */}
+                    <MenuProvider style={styles.moreButton}>
+                        <Menu>
+                            <MenuTrigger text="..." />
+                            <MenuOptions>
+                                <MenuOption onSelect={() => alert(`Save`)} text="Save" />
+                                <MenuOption onSelect={() => alert(`Delete`)}>
+                                    <Text style={{ color: 'red' }}>Delete</Text>
+                                </MenuOption>
+                                <MenuOption
+                                    onSelect={() => alert(`Not called`)}
+                                    disabled={true}
+                                    text="Disabled"
+                                />
+                            </MenuOptions>
+                        </Menu>
+                    </MenuProvider>
+                </View>
+                <TextInput
+                    style={styles.content}
+                    placeholder={content}
+                    onChangeText={content => this.setState({ content })}
+                    value={this.state.content}
+                />
+            </View>
+        );
+    }
+
+    openMore() {
+        console.log("MORE")
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#ddd',
+        // borderBottomWidth: 0,
+        // shadowColor: '#000',
+        // shadowOffset: { width: 0, height: 2 },
+        // shadowOpacity: 0.8,
+        // shadowRadius: 2,
+        elevation: 3,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+        width: 300,
+
+    },
+    topRow: {
+        flexDirection: 'row'
+    },
+    moreButton: {
+        alignItems: 'center',
+        // borderWidth: 1,
+        // elevation: 2,
+        // borderColor: 'rgba(52, 52, 52, 0.0)',
+        backgroundColor: 'rgba(200, 200, 200, 0.5)',
+        padding: 5,
+        flex: 5,
+    },
+    title: {
+        flex: 1,
+        fontWeight: 'bold',
+        fontSize: 16,
+        // top: 40,
+        // height: 40,
+        borderColor: 'gray',
+        borderBottomWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 5,
+    },
+    content: {
+        fontSize: 10,
+    },
+})
--- src/notesScreen.js
+++ src/notesScreen.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, ListView } from 'react-native';
+import { StyleSheet, View, Text, Button, ListView, FlatList, ListItem } from 'react-native';
 import firebase from '@firebase/app'
 import '@firebase/auth'
+import Note from './components/note'
 
 export default class NotesScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [{ title: 'Title', content: 'Enter note here...' }],
+      update: false
     }
-    this.dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1!==r2});
+    // this.dataSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
   }
 
   render() {
@@ -20,10 +22,32 @@ export default class NotesScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Welcome !</Text>
+        <FlatList
+          // data={this.state.data}
+          data={this.state.data}
+          extraData={this.state.update}
+          keyExtractor={(item, index) => index.toString()}
+          // renderItem={
+          //   ({item}) => <Text>{item.name}</Text>
+          //           } 
+          renderItem={({ item }) => (
+            <Note
+              // id={item.id}
+              // selected={!!this.state.selected.get(item.id)}
+              title={item.title}
+              content={item.content}
+            />
+          )
+          }
+        />
+        {/* <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => <ListItem title={item.title} />}
+        />
         <ListView
           dataSource={this.dataSource.cloneWithRows(this.state.data)}
           renderRow={(rowData) => <Text>{rowData}</Text>}
-        />
+        /> */}
         <Button
           title={"Add Note"}
           onPress={() => this.addNote()}
@@ -34,7 +58,9 @@ export default class NotesScreen extends React.Component {
 
   addNote() {
     console.log("Add Note!")
-    this.state.data.push("anotha one")
+    this.state.data.push({ title: 'Title', content: 'Enter note here...' })
+    // Toggle this to tell the list to update
+    this.setState({update: !this.state.update})
     console.log(this.state.data)
   }
 }
